f9206cdeeeba249ef86b67af4d8158a934aacfcc
suda_code <suda_code>
2024-06-30 22:53:24 +0800

    我夹带点私货不过分吧

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,8 @@
+// Copyright © 2024 OpenRapid. All rights reserved.
+console.log("Personal UI dev.0.0.1")
+// 夹 带 私 货
+console.log("by OpenRapid. No Wing, No Ahead")
+console.log("Load avatar, icon")
+
 import "./avatar.js"
 import "./icon.js"
